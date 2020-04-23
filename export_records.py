0e1da96e5fb0f320cf99b6f822a6d280757f7c51
johndickson5 <43122255+johndickson5@users.noreply.github.com>
2020-04-22 20:04:28 -0400

    Toyed with subfield support

--- export_records.py
+++ export_records.py
@@ -5,18 +5,30 @@ punct = ".!\"#$%&'()*+,-/:;<=>?@[\\]^_`{|}~ʹ·♭®±ʺ£°℗©♯¿€"
 
 while True:
     field = input('Enter a field number: ')
-    print(field)
     if not field.isdigit() or int(field) > 999:
         print("Not a valid MARC field!")
         continue
     else:
         break
 
+field_list = ['015', '020', '024', '027', '028', '210', '222']
+
+count = 0
+
 # Change this to whatever file you want to export marc records from
-with open('input_mrc/', 'rb') as fh:
+with open('input_mrc/input.mrc', 'rb') as fh:
     # Open the record and encode as unicode, to prevent errors
     reader = MARCReader(fh, to_unicode=True, force_utf8=True)
-    for record in reader:
-        if record[field]:
-            with open('output_mrc/' + field + '.mrc', 'ab') as out:
-                out.write(record.as_marc())
+    try:
+        for record in reader:
+            if record[field]:
+                with open('output_mrc/' + field + '.mrc', 'ab') as out:
+                    try:
+                        out.write(record.as_marc())
+                        count += 1
+                    except UnicodeDecodeError as e:
+                        print(e)
+    except:
+        pass
+
+print(f"{count} fields with this subfield.")
